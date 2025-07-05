5c97ffec3a65de544e85dd92e5ad9b8d82edc7f9
Artu2400 <artirajmane172@gmail.com>
2025-07-05 14:00:49 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int no=10;
+	printf("enter any number");
+	scanf("%d",&no);
+	if(no%2==0)
+	{
+		printf("number is even");
+	}
+	else
+	{
+		printf("number is odd");
+	}
+}
+
+
