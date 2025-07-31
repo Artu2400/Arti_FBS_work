5135cb2b9b7d1e2fce32e0b6f31d4289d7d1e649
Artu2400 <artirajmane172@gmail.com>
2025-07-31 23:12:56 +0530

    Add files via upload

---
+++ stuct employe demo 2.c
@@ -0,0 +1,31 @@
+//Employee (id, name, salary)
+#include<stdio.h>
+struct employee
+{
+	int id;
+	char name[10];
+	int salary;	
+};
+void main()
+{
+	struct employee s1,s2,s3;
+	printf("enter the employee id \n");
+	scanf("%d",&s1.id);
+	printf("enter the employee name \n");
+	scanf("%s",&s1.name);
+	printf("enter the employee salary \n");
+	scanf("%d",&s1.salary);	
+	
+	printf("enter the employee id \n");
+	scanf("%d",&s2.id);
+	printf("enter the employee name \n");
+	scanf("%s",&s2.name);
+	printf("enter the employee salary \n");
+	scanf("%d",&s2.salary);	
+	
+	
+
+
+	
+	
+}
